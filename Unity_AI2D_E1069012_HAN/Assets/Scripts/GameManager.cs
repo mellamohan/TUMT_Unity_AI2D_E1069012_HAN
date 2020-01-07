f1f612c9892de5b783a6d358c7e14835d30f3b75
mellamohan <55227102+mellamohan@users.noreply.github.com>
2020-01-07 12:07:59 +0800

    0107

--- Unity_AI2D_E1069012_HAN/Assets/Scripts/GameManager.cs
+++ Unity_AI2D_E1069012_HAN/Assets/Scripts/GameManager.cs
@@ -47,4 +47,14 @@ public class GameManager : MonoBehaviour
             }
         }
     }
+     
+    public void Replay()
+    {
+        SceneManager.LoadScene("選單");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
--- Unity_AI2D_E1069012_HAN/Assets/Scripts/Player.cs
+++ Unity_AI2D_E1069012_HAN/Assets/Scripts/Player.cs
@@ -6,12 +6,31 @@ public class Player : MonoBehaviour
     public int HP = 100;
     public Slider HPSlider;
 
-    public Text textITEM;
+    public Text textICON;
     public int ICONCount;
     public int ICONTotal;
 
     public Text textTime;
     public float gameTime;
+
+    public GameObject final;
+    public Text textBest;
+    public Text textCurrent;
+
+    public Text TextICON
+    {
+       get
+        {
+          return textICON;
+        }
+
+       set
+       {
+          textICON = value;
+       }
+    }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -21,18 +40,19 @@ public class Player : MonoBehaviour
             int d = other.GetComponent<Trap>().damage;
             HP -= d;
             HPSlider.value = HP;
+            if (HP <= 0) Dead();
         }
 
         if (other.tag == "ICON")
         {
             ICONCount++;
-            textITEM.text = "ICON : " + ICONCount + " / " + ICONTotal;
+            textICON.text = "ICON : " + ICONCount + " / " + ICONTotal;
             Destroy(other.gameObject);
         }
 
         if (other.name == "終點" && ICONCount == ICONTotal)
         {
-            print("過關");
+            print("GAME OVER");
         }
     }
 
@@ -43,13 +63,19 @@ public class Player : MonoBehaviour
             int d = other.GetComponent<Trap>().damage;
             HP -= d;
             HPSlider.value = HP;
+            if (HP <= 0) Dead();
         }
     }
 
-    private void Start()
+    public void Start()
     {
+        if (PlayerPrefs.GetFloat("BEST") == 0)
+        {
+            PlayerPrefs.SetFloat("BEST", 999999);
+        }
+
         ICONTotal = GameObject.FindGameObjectsWithTag("ICON").Length;
-        textITEM.text = "ICON : 0 / " + ICONTotal;
+        textICON.text = "ICON : 0 / " + ICONTotal;
     }
 
     private void Update()
@@ -62,4 +88,31 @@ public class Player : MonoBehaviour
         gameTime += Time.deltaTime;
         textTime.text = gameTime.ToString("F2");
     }
+     
+    private void Dead()
+    {
+        final.SetActive(true);
+        textCurrent.text = "TIME : " + gameTime.ToString("F2");
+        textBest.text = "BEST" + PlayerPrefs.GetFloat("BT").ToString("F2");
+        Cursor.lockState = CursorLockMode.None;
+
+        GetComponent<FPSControllerLPFP.FpsControllerLPFP>().enabled = false;
+        enabled = false;
+    }
+     
+    private void GameOver()
+    {
+        final.SetActive(true);
+        textCurrent.text = "TIME : " + gameTime.ToString("F2");
+         
+        if (gameTime < PlayerPrefs.GetFloat("BT"))
+        {
+            PlayerPrefs.SetFloat("BT", gameTime);
+        }
+
+        textBest.text = "BEST" + PlayerPrefs.GetFloat("BT").ToString("F2");
+
+        Cursor.lockState = CursorLockMode.None;
+    }
+
 }
